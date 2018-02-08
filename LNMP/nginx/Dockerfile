8b296188dc9f0cecc75e9f032a812d1d8b538cd5
unclepang55 <10846295@qq.com>
2018-02-08 10:06:37 +0800

    a

--- LNMP/nginx/Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:1.11
-
-ENV TZ=Asia/Shanghai
-COPY ./nginx.conf /etc/nginx/conf.d/default.conf
---
+++ LNMP/src/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xiaobopang
+ * Date: 2018/2/7
+ * Time: 17:37
+ */
+
+echo phpinfo();
